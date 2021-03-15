900e055128c188f8ff05da687c06b41516f826c2
Maniek <mariusz.aleszko@gmail.com>
2021-03-15 18:48:35 +0100

    fixed bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const fs = require("fs");
-const ccxt = require("ccxt");
+require('dotenv').config();
+const fs = require('fs');
+const ccxt = require('ccxt');
 
 const apiKey = process.env.APIKEY;
 const secret = process.env.SECRET;
@@ -9,7 +9,8 @@ const exchangeName = process.env.EXCHANGE;
 const amount = process.env.AMOUNT;
 const estimatedProfit = process.env.ESTIMATED_PROFIT;
 const ticker = process.argv[2];
-const symbol = `${ticker}/BTC`;
+const coin = process.env.COIN;
+const symbol = `${ticker}/${coin}`;
 
 const exchange = new ccxt[exchangeName]({
   apiKey,
@@ -21,15 +22,15 @@ const params = {
   quoteOrderQty: amount,
 };
 
-const file = fs.readFileSync("marketInfo.json");
+const file = fs.readFileSync('marketInfo.json');
 const marketInfo = JSON.parse(file);
 
 (async function () {
   const { amount: precisionAmount, price: precisionPrice } = marketInfo[symbol].precision;
   const { taker } = marketInfo[symbol];
-  const { amount, trades } = await exchange.createOrder(symbol, "market", "buy", undefined, undefined, params);
+  const { amount, trades } = await exchange.createOrder(symbol, 'market', 'buy', undefined, undefined, params);
   const amountToSell = amount * (1 - taker).toFixed(precisionAmount);
   const price = (trades[trades.length - 1].price * estimatedProfit).toFixed(precisionPrice);
-  const sell = await exchange.createOrder(symbol, "limit", "sell", amountToSell, price);
+  const sell = await exchange.createOrder(symbol, 'limit', 'sell', amountToSell, price);
   console.log(sell);
 })();
